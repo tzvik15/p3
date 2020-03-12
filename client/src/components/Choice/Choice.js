760128915f1b553feb19fd77dc8938b66e4d23f1
waterylime46@aol.com <waterylime46@aol.com>
2020-03-11 18:29:02 -0600

    styling for at lease some of the com

--- client/src/components/Choice/Choice.js
+++ client/src/components/Choice/Choice.js
@@ -9,7 +9,6 @@ const Choice = (props) => {
 				<CardBody>
 					{/* <CardTitle>Card title</CardTitle>
             <CardSubtitle>Card subtitle</CardSubtitle> */}
-<<<<<<< HEAD
 					<CardText>How many spaces would you like to move?</CardText>
 					<Button className="moveBtn" onClick={props.moveOne}>
 						Move one space
@@ -24,16 +23,6 @@ const Choice = (props) => {
 			</Card>
 		</div>
 	);
-=======
-            <CardText>How many spaces would you like to move?</CardText>
-            <Button className="moveBtn" onClick={props.moveOne}>Move one space</Button>
-            <Button className="moveBtn" onClick={props.moveTwo}>Move two spaces</Button>
-            <Button className="moveBtn" onClick={props.moveThree}>Move three spaces</Button>
-         </CardBody>
-         </Card>
-      </div>
-   );
->>>>>>> 7f13e56a241214ce4b0452f17bee2a7385d124f7
 };
 
 export default Choice;
