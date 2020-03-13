a1e39cbb2a3466817cffc5c22c649073b4fedc2f
Clyde Plasencia <cquiatt@gmail.com>
2020-03-13 17:08:14 -0600

    update to server.js mlabdb url

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ if (process.env.NODE_ENV === "production") {
 //   }
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/cards",
+  process.env.MONGODB_URI || "mongodb://user:tzvik200@ds147900.mlab.com:47900/heroku_bh00mj12",
   { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true },
   console.log("Db connected!")
 );
