93a234f9fe7ceabc7560f200993f3612906feb92
Daniel Nester <tzvik15@yahoo.com>
2020-03-18 09:27:09 -0600

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -10,8 +10,8 @@ import socketIOClient from "socket.io-client";
 import CardContext from "./utils/CardContext";
 import Footer from "./components/footer/Footer";
 
-import io from "socket.io-client";
-let socket;
+//import io from "socket.io-client";
+//let socket;
 
 function App() {
   window.pNum = 0;
@@ -38,7 +38,7 @@ function App() {
   // }
 
   const [textValue, setTextValue] = useState("");
-  const socket = socketIOClient("https://radiant-retreat-86258.herokuapp.com/");
+  const socket = socketIOClient("http://127.0.0.1:4001");
   
   //const ENDPOINT = "https://radiant-retreat-86258.herokuapp.com/";
   //socket = io(ENDPOINT);
