15d51f99d58c765adbf9751d504731eb4eae1fce
Daniel Nester <tzvik15@yahoo.com>
2020-03-23 11:17:03 -0600

    Update server.js

--- server.js
+++ server.js
@@ -31,8 +31,14 @@ if (process.env.NODE_ENV === "production") {
 //   console.log("Db connected!")
 // );
 
+// mongoose.connect(
+//   process.env.MONGODB_URI || "mongodb://user:tzvik200@ds147900.mlab.com:47900/heroku_bh00mj12",
+//   { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true },
+//   console.log("Db connected!")
+// );
+
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://user:tzvik200@ds147900.mlab.com:47900/heroku_bh00mj12",
+  process.env.MONGODB_URI || "mongodb://localhost/cards",
   { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true },
   console.log("Db connected!")
 );
